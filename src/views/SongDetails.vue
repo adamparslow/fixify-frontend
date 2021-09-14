d0fb955b0431b1f76545216246aaad7221b2bc13
adamparslow <adam.parslow@hotmail.com>
2021-09-14 21:16:00 +1000

    Add song detail inforamtion

--- src/views/SongDetails.vue
+++ src/views/SongDetails.vue
@@ -2,6 +2,17 @@
     <div class="flex flex-col">
         <router-link to="/search">Back</router-link>
         {{$route.params.id}}    
+
+        <hr>
+        <h2 class="text-2xl">Playlists the Song is in</h2>
+        <p v-for="(playlist, i) in playlists" v-bind:key="i">{{playlist}}</p>
+
+        <hr>
+        <h2 class="text-2xl">Audio Features</h2>
+        <p v-for="(feature, i) in Object.keys(audioFeatures)" v-bind:key="i">{{feature}}:{{audioFeatures[feature]}}</p>
+
+        <hr>
+        <p>You liked this song on {{formatAddedAt}}</p>
     </div>
 </template>
 
@@ -9,9 +20,24 @@
 import spotifyService from "@/model/spotifyService.js";
 
 export default {
+    data: function () {
+        return {
+            playlists: [],
+            audioFeatures: {},
+            addedAt: new Date()
+        }
+    },
+    computed: {
+        formatAddedAt: function() {
+            return `${this.addedAt.getDate()}/${this.addedAt.getMonth() + 1}/${this.addedAt.getFullYear()}`;
+        }
+    },
     created: async function() {
         const response = await spotifyService.getSongData(this.$route.params.id);
-        console.log(response);
+
+        this.playlists = response.playlists
+        this.audioFeatures = response.audioFeatures;
+        this.addedAt = new Date(response.addedAt);
     }
 }
 </script>
